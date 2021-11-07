cef1fd4a1f835f8332dd40c851342f2c110b8150
emilyflora <e.kyne@outlook.com>
2021-11-07 15:44:35 -0600

    built grid and basic function structure

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // on page load, generate 16x16 grid of divs inside div .container
 
+function createGrid(n) {
+    let num = n*n;
+    const container = document.querySelector(".container");
+        for (i=0; i<num; i++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            container.appendChild(cell);
+            cell.setAttribute("style", "width: " + 400/n + "px; height: " + 400/n + "px;");
+    };
+    container.setAttribute("style", "grid-template-columns: repeat(" + n +", 1fr);");
+};
+
+createGrid(32);
