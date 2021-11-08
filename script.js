2c979eea67d6ca3f93ca930e97a9e0d63026cce3
emilyflora <e.kyne@outlook.com>
2021-11-08 12:08:38 -0600

    completed functional first iteration

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-// on page load, generate 16x16 grid of divs inside div .container
 
+    
+// on page load, generate 16x16 grid of divs inside div .container
 function createGrid(n) {
     let num = n*n;
     const container = document.querySelector(".container");
         for (i=0; i<num; i++) {
-            const cell = document.createElement("div");
+            let cell = document.createElement("div");
             cell.classList.add("cell");
             container.appendChild(cell);
-            cell.setAttribute("style", "width: " + 400/n + "px; height: " + 400/n + "px;");
+            // let cell = document.querySelector(".cell");
+            // let dimensions = 400/n + " px;";
+            // cell.style.width = dimensions; 
+            // cell.style.height = dimensions;
+            // cell.setAttribute("style", "width: " + 400/n + "px; height: " + 400/n + "px;");
     };
-    container.setAttribute("style", "grid-template-columns: repeat(" + n +", 1fr);");
+    // container.setAttribute("style", "grid-template-columns: repeat(" + n +", 1fr);");
 };
 
-createGrid(32);
+// on hover, change .cell background color
+function changeColor() {
+    let cellStyle = document.querySelectorAll(".cell");
+    for (let i=0; i<cellStyle.length; i++) {
+        let cell = cellStyle[i];
+        cell.addEventListener("mouseover", function () {
+            cell.classList.add("hover-black");
+        });
+    };
+};
+    
+
+//running functions
+let n = 32;
+
+function runAll() {
+    createGrid(n);
+    changeColor();
+};
+
+runAll();
